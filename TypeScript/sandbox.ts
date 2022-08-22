a31b1f0479f9e67dff2e279f7b5242273b56d150
Jeff <jbridges1119@gmail.com>
2022-08-22 13:33:54 -0600

    Testing restrictions

--- TypeScript/sandbox.ts
+++ TypeScript/sandbox.ts
@@ -1,11 +1,24 @@
-const character = "mario"
+let names = ["luigi", "mario", "yoshi"];
 
-console.log(character);
+// names.push(3);
 
-const inputs = document.querySelectorAll('input');
+let numbers = [1, 2, 3, 4, 5];
 
-console.log(inputs);
+// numbers.push("shawn");
 
-inputs.forEach(input => {
-  console.log(input);
-});
+let mixed = ["ken", 3, "chun-li", 6, 4];
+mixed.push("ryu");
+mixed.push(4);
+mixed[0] = 3;
+
+let mySelf = {
+  name: "Jeff",
+  color: "Green",
+  age: 36,
+};
+
+mySelf.age = 37
+mySelf.name = 'Jeff Bridges'
+
+// mySelf.age = '36'
+// mySelf.knowledge = ['Bowhunting skills', 'Computer Hacking Skills']
