043ef47d96d7c9dda8080d370f7b295c876dc476
Jeff <jbridges1119@gmail.com>
2022-08-23 16:22:34 -0600

    Change script file to app.js

--- public/sandbox.js
+++ public/sandbox.js
@@ -33,7 +33,7 @@ let house = 25;
 let greet;
 //greet = 'hello';
 greet = () => {
-    console.log('hello, again');
+    console.log("hello, again");
 };
 //returns nothing or void
 const add = (a, b, c) => {
@@ -43,4 +43,28 @@ add(5, 4);
 //returns a number
 const minus = (a, b) => {
     return a + b;
+};
+const logDetails = (uid, item) => {
+    console.log(`${item} has a uid of ${uid}`);
+};
+const greeting = (user) => {
+    console.log(`${user.name} says hello`);
+};
+//return void function
+// () => void
+//2 parameters (can be anything not just a and b) that returns void
+let greet1;
+//2 parameters that return void
+greet1 = (name, greeting) => {
+    console.log(`${name} says ${greeting}`);
+};
+let calc;
+//else must be there or we wouldn't return a number
+calc = (numOne, numTwo, action) => {
+    if (action === 'add') {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
 };
